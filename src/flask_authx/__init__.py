e2e1505bf87516244b7c970dc6773ce145bfb6af
kronnor919 <segundacuenta7364@gmail.com>
2026-01-26 05:35:26 -0500

    chore: inital commit

---
+++ src/flask_authx/__init__.py
@@ -0,0 +1,6 @@
+from flask_authx.extension import AuthX as AuthX
+from flask_authx.extension import AuthXBuilder as AuthXBuilder
+from flask_authx.interfaces.database import IDatabaseSetup as IDatabaseSetup
+from flask_authx.interfaces.repository import ISessionsRepository as ISessionsRepository
+from flask_authx.interfaces.repository import IUsersRepository as IUsersRepository
+from flask_authx.interfaces.service import IAuthService as IAuthService
