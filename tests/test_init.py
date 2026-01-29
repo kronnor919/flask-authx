8a4d953c1ff7db76d4e1f161b33a8d50fd0d15ba
kronnor919 <segundacuenta7364@gmail.com>
2026-01-29 07:38:20 -0500

    tests(configuration): extract the extension configuration in the app to a separate fixture

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,16 +1,34 @@
+import pytest
 from flask import Flask
 from flask_authx import AuthXBuilder
 
 
-def test_init(app: Flask):
-    authx = AuthXBuilder(app).build()
+USERNAME = "kronnor"
+PW = "73336463"
 
-    rules = [r.rule for r in app.url_map.iter_rules()]
 
-    with app.app_context():
-        admin_res = authx.users_repository.get_by_name("admin")  # pyright: ignore[reportOptionalMemberAccess]
+@pytest.fixture(scope="module")
+def builded_app(app: Flask):
+    app_copy = app
 
-    assert authx == app.extensions["authx"]
+    app_copy.config.from_mapping(
+        {"FIRST_USER_NAME": USERNAME, "FIRST_USER_PASSWORD": PW}
+    )
+
+    AuthXBuilder(app_copy).build()
+
+    yield app_copy
+
+
+def test_init(builded_app: Flask):
+    authx = builded_app.extensions["authx"]
+
+    rules = [r.rule for r in builded_app.url_map.iter_rules()]
+
+    with builded_app.app_context():
+        admin_res = authx.users_repository.get_by_name(USERNAME)  # pyright: ignore[reportOptionalMemberAccess]
+
+    assert authx == builded_app.extensions["authx"]
     assert "/users" in rules
     assert "/auth/login" in rules
     assert "/auth/logout" in rules
