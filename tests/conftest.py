735c8596ae5367e808de91760959c65b5c514a17
kronnor919 <segundacuenta7364@gmail.com>
2026-01-29 07:16:44 -0500

    tests(configuration): remove unused fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,3 @@
-import os
 from pytest import fixture
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -19,15 +18,3 @@ def app():
     SQLAlchemy(app)
 
     return app
-
-
-@fixture(autouse=True)
-def env_config():
-    env_path = ".env"
-
-    with open(env_path, "w") as f:
-        f.writelines(['SUPERUSER_NAME="admin"\n', 'SUPERUSER_PASSWORD="14/10/10"'])
-
-    yield
-
-    os.remove(env_path)
