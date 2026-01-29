8525b1b0bf03b2d1da7d8a31646d4aa95ea1dfd6
kronnor919 <segundacuenta7364@gmail.com>
2026-01-29 07:51:25 -0500

    tests(configuration): verify that the configuration was set correctly in the extension and that the first admin is created with the expected credentials

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,6 +1,8 @@
 import pytest
 from flask import Flask
 from flask_authx import AuthXBuilder
+from flask_authx import AuthX
+from flask_authx import config
 
 
 USERNAME = "kronnor"
@@ -21,7 +23,7 @@ def builded_app(app: Flask):
 
 
 def test_init(builded_app: Flask):
-    authx = builded_app.extensions["authx"]
+    authx: AuthX = builded_app.extensions["authx"]
 
     rules = [r.rule for r in builded_app.url_map.iter_rules()]
 
@@ -33,3 +35,22 @@ def test_init(builded_app: Flask):
     assert "/auth/login" in rules
     assert "/auth/logout" in rules
     assert admin_res.value is not None
+
+
+def test_config():
+    assert config.FIRST_USER_NAME == USERNAME
+    assert config.FIRST_USER_PASSWORD == PW
+
+
+def test_first_user_exists(builded_app: Flask):
+    authx: AuthX = builded_app.extensions["authx"]
+
+    with builded_app.app_context():
+        user_res = authx.users_repository.get_by_name(USERNAME)  # pyright: ignore[reportOptionalMemberAccess]
+
+    assert user_res.success is True
+
+    admin = user_res.value
+
+    assert admin.username == USERNAME
+    assert authx.password_hashing.verify(PW, admin.password)  # pyright: ignore[reportOptionalMemberAccess]
