ddce32e8e9755da176cffece5e59b9d6e10056de
kronnor919 <segundacuenta7364@gmail.com>
2026-02-12 13:23:12 -0500

    feat(api): expose entities, forms, services and errors
    Update __init__.py to export core entities, forms, services, and error classes. This simplifies imports for library users by exposing the public API directly from the top-level package.

--- src/flask_authx/__init__.py
+++ src/flask_authx/__init__.py
@@ -3,5 +3,52 @@ from flask_authx.extension import AuthXBuilder as AuthXBuilder
 from flask_authx.interfaces.database import IDatabaseSetup as IDatabaseSetup
 from flask_authx.interfaces.repository import ISessionsRepository as ISessionsRepository
 from flask_authx.interfaces.repository import IUsersRepository as IUsersRepository
-from flask_authx.interfaces.service import IAuthService as IAuthService
 from flask_authx.interfaces.security import IPasswordHashing as IPasswordHashing
+from flask_authx.interfaces.service import IAuthService as IAuthService
+from flask_authx.interfaces.service import IUsersService as IUsersService
+from flask_authx.entities import User as User
+from flask_authx.entities import Session as Session
+from flask_authx.forms import UserForm as UserForm
+from flask_authx.forms import UserUpdateData as UserUpdateData
+from flask_authx.forms import SessionForm as SessionForm
+from flask_authx.role import Role as Role
+from flask_authx.utils import responses as responses
+from flask_authx.errors import ProgrammingError as ProgrammingError
+from flask_authx.errors import AppError as AppError
+from flask_authx.errors import ConfigurationError as ConfigurationError
+from flask_authx.errors import NotFoundError as NotFoundError
+from flask_authx.errors import ValidationError as ValidationError
+from flask_authx.errors import DatabaseError as DatabaseError
+from flask_authx.errors import ConflictError as ConflictError
+from flask_authx.errors import DependentResourceError as DependentResourceError
+from flask_authx.errors import NotAuthenticatedError as NotAuthenticatedError
+from flask_authx.errors import InvalidCredentialsError as InvalidCredentialsError
+
+
+__all__ = [
+    "AuthX",
+    "AuthXBuilder",
+    "IDatabaseSetup",
+    "ISessionsRepository",
+    "IUsersRepository",
+    "IAuthService",
+    "IPasswordHashing",
+    "IUsersService",
+    "User",
+    "Session",
+    "UserForm",
+    "UserUpdateData",
+    "SessionForm",
+    "Role",
+    "responses",
+    "ProgrammingError",
+    "AppError",
+    "ConfigurationError",
+    "NotFoundError",
+    "ValidationError",
+    "DatabaseError",
+    "ConflictError",
+    "DependentResourceError",
+    "NotAuthenticatedError",
+    "InvalidCredentialsError",
+]
